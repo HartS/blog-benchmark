b779bac677ee3b67b71a253879af74fb14705c8b
Hart Simha <hmsimha@gmail.com>
2021-02-09 17:38:51 -0500

    Add gatsby blog

---
+++ gatsby-md-blog/gatsby-config.js
@@ -0,0 +1,27 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+    siteMetadata: {
+        title: `The Markdown Guide`,
+        description: `This is a guide to "Markdown"`
+    },
+    plugins: [
+        {
+            resolve: `gatsby-plugin-mdx`,
+            options: {
+                extensions: [`.mdx`, `.md`],
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/posts`,
+            },
+        },
+    ],
+}
