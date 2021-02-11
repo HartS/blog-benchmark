00a4494e470a74141ba53e6e0428edd68f20fb7b
Hart Simha <hmsimha@gmail.com>
2021-02-10 23:26:01 -0500

    Add gridsome

---
+++ gridsome/gridsome.config.js
@@ -0,0 +1,20 @@
+// This is where project configuration and plugin options are located.
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'Gridsome Blog Guide Blog',
+  siteDescription: 'Gridsome-powered blog with Markdown Guide for blog-benchmarks',
+  plugins: [
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        path: 'content/posts/**/*.md',
+        typeName: 'Post',
+        route: '/blog/:slug'
+      }
+    }
+  ]
+}
